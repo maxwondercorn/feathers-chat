f96bb7da91e28cc38f0ced0dc5e54b09cf4e40fb
David Luecke <daff@neyeon.com>
2019-04-21 16:25:58 -0700

    Upgrade chat application to Feathers 4 prerelease

--- src/authentication.js
+++ src/authentication.js
@@ -1,27 +1,11 @@
-const authentication = require('@feathersjs/authentication');
-const jwt = require('@feathersjs/authentication-jwt');
-const local = require('@feathersjs/authentication-local');
-
+const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
+const { LocalStrategy } = require('@feathersjs/authentication-local');
 
 module.exports = function (app) {
-  const config = app.get('authentication');
+  const authentication = new AuthenticationService(app);
 
-  // Set up authentication with the secret
-  app.configure(authentication(config));
-  app.configure(jwt());
-  app.configure(local());
+  authentication.register('jwt', new JWTStrategy());
+  authentication.register('local', new LocalStrategy());
 
-  // The `authentication` service is used to create a JWT.
-  // The before `create` hook registers strategies that can be used
-  // to create a new valid JWT (e.g. local or oauth2)
-  app.service('authentication').hooks({
-    before: {
-      create: [
-        authentication.hooks.authenticate(config.strategies)
-      ],
-      remove: [
-        authentication.hooks.authenticate('jwt')
-      ]
-    }
-  });
+  app.use('/authentication', authentication);
 };
